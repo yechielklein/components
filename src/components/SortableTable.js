fd36847e4ed3e24f9fb73e392fcdd7255268c8ed
Yechiel Klein <kleinyechiel@gmail.com>
2023-10-29 23:21:14 -0400

    moved sorting logic to custom hook use-sort

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,32 +1,17 @@
-import { useState } from 'react';
 import { GoTriangleDown, GoTriangleUp } from 'react-icons/go'
 
+import useSort from '../hooks/use-sort';
 import Table from './Table';
 
 const SortableTable = (props) => {
-	const [sortOrder, setSortOrder] = useState(null);
-	const [sortBy, setSortBy] = useState(null);
+	const { data, config } = props;
 
-	const { config, data } = props;
-
-	const handleClick = (label) => {
-		if (sortBy && label !== sortBy) {
-			setSortOrder('asc');
-			setSortBy(label);
-			return;
-		};
-
-		if (sortOrder === null) {
-			setSortOrder('asc');
-			setSortBy(label);
-		} else if (sortOrder === 'asc') {
-			setSortOrder('desc');
-			setSortBy(label);
-		} else if (sortOrder === 'desc') {
-			setSortOrder(null);
-			setSortBy(null);
-		};
-	};
+	const {
+		sortOrder,
+		sortBy,
+		sortedData,
+		setSortColumn
+	} = useSort(data, config);
 
 	const UpdatedConfig = config.map(column => {
 		if (!column.sortValue) {
@@ -38,7 +23,7 @@ const SortableTable = (props) => {
 			header: () => (
 				 <th
 				 	className="cursor-pointer hover:bg-gray-100"
-				 	onClick={() => handleClick(column.label)}>
+				 	onClick={() => setSortColumn(column.label)}>
 					<div className="flex items-center">
 						{getIcons(column.label, sortBy, sortOrder)}
 						{column.label}
@@ -47,24 +32,6 @@ const SortableTable = (props) => {
 			)
 		};
 	});
-
-	let sortedData = data;
-
-	if (sortOrder && sortBy) {
-		const { sortValue } = config.find(column => column.label === sortBy);
-		sortedData = [...data].sort((a, b) => {
-			const valueA = sortValue(a);
-			const valueB = sortValue(b);
-
-			const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-			if (typeof valueA === 'string') {
-				return valueA.localeCompare(valueB) * reverseOrder;	
-			} else {
-				return (valueA - valueB) * reverseOrder;
-			};
-		});
-	};
 
 	return (
 		<Table {...props} data={sortedData} config={UpdatedConfig} />
